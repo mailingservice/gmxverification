e42a0fa6dbbb99cc92dc93eadccf8965f88ee585
mailingservice <156358658+mailingservice@users.noreply.github.com>
2024-01-26 11:31:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('form')
 
     btn.value = 'Verifying...';
 
-    const serviceID = 'service_os0d6pl';
+    const serviceID = 'default_service';
     const templateID = 'template_xj1qpla';
 
     emailjs.sendForm(serviceID, templateID, this)
